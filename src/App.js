728cc33d817a3c92be1babca60d3f43c1e0e869d
JoaPerss <joachimlp@hotmail.no>
2022-11-17 00:34:04 +1000

    Remove particles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import Particles from "./components/Particles";
 
 const App = () => {
     return (
-    <Particles />
-)};
+        <div >
+            <h1>App</h1>
+            </div>)};
 
 export default App;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,10 @@
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+            <h1>My Sidebar</h1>
+
+        </div>
+    )
+}
+
+export default Sidebar
