f291f51ac7555df5ae5f11f651420867e32263d3
JoaPerss <joachilp@hiof.no>
2023-01-16 16:30:42 +0100

    Added skills

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import Main from './components/Main';
 import Info from './components/Info';
 import Projects from './components/Projects';
-import Skills from './components/Skills';
+import SkillsImage from './components/SkillsImage';
 import React from "react";
 
 const aboutMe = "I am a 22 year old fullstack developer from Norway. I have a bachelors degree in Information Systems from Østfold University College. My degree was focused on Software Engineering and Business Intelligence. I have broad expirence in development in teams and solo on projects. This includes modern web apps, data science case studies, mobile apps and business analysis.";
+const skillsImage = ["https://www.freepnglogos.com/uploads/html5-logo-png/html5-logo-devextreme-multi-purpose-controls-html-javascript-3.png",
+"https://logos-download.com/wp-content/uploads/2016/09/React_logo_wordmark.png",
+"https://www.citypng.com/public/uploads/small/11662223208mrscxxesmsb91zq9ubngbriqcijzc1j6q5u12serbmv9pimrziktl695capupvzdmhwhn7tozqooqkhcuxw9cejvc0fia2foatgb.png",
+"https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Microsoft_.NET_logo.svg/2048px-Microsoft_.NET_logo.svg.png",
+"https://upload.wikimedia.org/wikipedia/labs/8/8e/Mysql_logo.png", 
+"https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/1200px-Postgresql_elephant.svg.png",
+"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/590px-Node.js_logo.svg.png",
+"https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Expressjs.png/1200px-Expressjs.png",
+"https://www.pngmart.com/files/13/Android-Logo-PNG-Transparent-Image.png",
+"https://www.logo.wine/a/logo/Python_(programming_language)/Python_(programming_language)-Logo.wine.svg",
+"https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Git_icon.svg/1200px-Git_icon.svg.png",
+"https://upload.wikimedia.org/wikipedia/commons/thumb/e/ed/Pandas_logo.svg/2560px-Pandas_logo.svg.png",
+"https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Amazon_Web_Services_Logo.svg/1024px-Amazon_Web_Services_Logo.svg.png",
+"https://www.docker.com/wp-content/uploads/2022/03/vertical-logo-monochromatic.png",
+"https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/2367px-Vue.js_Logo_2.svg.png ",
+"https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Cassandra_logo.svg/1024px-Cassandra_logo.svg.png",
+]
 
 const App = () => {
     return (
         <div>
             <Main />
             <Info info={aboutMe} />
+            <SkillsImage skillsImage={skillsImage} />
             <Projects />
-            <Skills />
         </div>
     );
 };
---
+++ src/components/SkillsImage.js
@@ -0,0 +1,23 @@
+//make a react component to display the skills as images that are links passed as props
+const SkillsImage = ({ skillsImage }) => {
+    return (
+        <div className="container mx-auto bg-peach w-4/5 p-5 rounded-lg opacity-98 h-max mt-5">
+            <h1 className="text-7xl text-center font-aloevera m-10">My Skills</h1>
+            <div className="place-content-center">
+            {ListMaker(skillsImage)}
+            </div>
+        </div>
+    );
+};
+
+export default SkillsImage;
+function ListMaker(skillsImage) {
+    return <ul className="flex flex-wrap gap-4 justify-between">
+        {skillsImage.map(skillImage => (
+            <li key={skillImage} className="text-2xl text-center font-aloevera m-10 max-w-4/5">
+                <img className="h-32" src={skillImage} alt="Pictures of skills" />
+            </li>
+        ))}
+    </ul>;
+}
+
